0701025e8952f1d9c9496ea98db2869cd2f42943
Grácouš <84779291+Graca98@users.noreply.github.com>
2024-02-20 13:35:16 +0100

    Přídání tlačítek na upravení a smazaní k ukolu + přidán tailwind do aplikace

--- js/todo.js
+++ js/todo.js
@@ -28,8 +28,20 @@ function createTask() {
     label.htmlFor = idUkolu
     label.textContent = todoInput.value
 
+    let editBtn = document.createElement("button")
+    editBtn.id = "edit" + idCislo
+    editBtn.className = yellowBtn
+    editBtn.textContent = "Upravit"
+
+    let smazatBtn = document.createElement("button")
+    smazatBtn.id = "smazat" + idCislo
+    smazatBtn.className = redBtn
+    smazatBtn.textContent = "Smazat"
+
     li.appendChild(label)
     li.appendChild(input)
+    li.appendChild(editBtn)
+    li.appendChild(smazatBtn)
     seznamUkolu.appendChild(li)
 
     zpracujUkol()
@@ -46,10 +58,19 @@ function zpracujUkol() {
     for (i = 1; i <= seznamUkolu.childElementCount; i++) {
         let ukol = document.getElementById("ukol" + i)
         ukol.onchange = () => {
-            console.log("Checkbox \"" + ukol.id + "\" je zakliknutý = " + ukol.checked);
+            if (ukol.checked) {
+                console.log("Checkbox \"" + ukol.id + "\" je zakliknutý = " + ukol.checked);
+            } else {
+                console.log("Checkbox \"" + ukol.id + "\" není zakliknutý = " + ukol.checked);
+            }
         }
     }
 }
+
+
+//? ========== Styly ==========
+let redBtn = "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+let yellowBtn = "focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
 
 
 //! ========== Testování =========
@@ -60,5 +81,4 @@ function splnUkolTest(id) {
     }
 }
 splnUkolTest(testUkol)
-//! ========== Testování =========
 
