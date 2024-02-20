631dc26f5a70edbb06aeb6b8176469bff4202554
Grácouš <84779291+Graca98@users.noreply.github.com>
2024-02-20 12:44:17 +0100

    Základní fungování
    JS reaguje na to, který ukol byl zakliknutý v konzoli

--- js/todo.js
+++ js/todo.js
@@ -8,11 +8,58 @@ todoButton.onclick = (e) => {
     createTask()
 }
 
+let idCislo = 0
 function createTask() {
     let seznamUkolu = document.getElementById("seznamUkolu")
 
-    let li = document.createElement("li")
-    li.textContent = todoInput.value
+    if (idCislo === 0) {
+        idCislo = 1
+    } else {
+        idCislo++
+    }
 
+    let idUkolu = "ukol" + idCislo
+
+    let li = document.createElement("li")
+    let input = document.createElement("input")
+    input.type = "checkbox"
+    input.id = idUkolu
+    let label = document.createElement("label")
+    label.htmlFor = idUkolu
+    label.textContent = todoInput.value
+
+    li.appendChild(label)
+    li.appendChild(input)
     seznamUkolu.appendChild(li)
+
+    splnUkolTest()
 }
+
+function najdiUkol() {
+    let seznamUkolu = document.getElementById("seznamUkolu")
+    console.log("Počet úkolů " + seznamUkolu.childElementCount);
+    console.log("blabla je: " + seznamUkolu.childElementCount);
+}
+
+let testUkol = document.getElementById("ukol0")
+function splnUkol(id) {
+    id.onchange = () => {
+        console.log("Checkbox \"" + id.id + "\" je zakliknutý = " + id.checked);
+    }
+}
+
+function splnUkolTest() {
+    let seznamUkolu = document.getElementById("seznamUkolu")
+    for (i = 1; i <= seznamUkolu.childElementCount; i++) {
+        let ukol = document.getElementById("ukol" + i)
+        ukol.onchange = () => {
+            console.log("Checkbox \"" + ukol.id + "\" je zakliknutý = " + ukol.checked);
+        }
+
+    }
+    // id.onchange = () => {
+    //     console.log("Checkbox je zakliknutý = " + id.checked);
+    // }
+}
+
+splnUkol(testUkol)
